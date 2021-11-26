29c2644b81006923dbf0c8a10171d7ee2091dc4e
Hayden Heroux <haydenheroux@gmail.com>
2021-11-26 17:34:20 -0500

    Write "Test" to the file of the next ticket.
    Signed-off-by: Hayden Heroux <haydenheroux@gmail.com>

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <err.h>
 
 #include "arg.h"
 #include "io.h"
 
-int main(int argc, const char** argv) {
+#define BUFSIZE 80
+
+int main() {
+	int num = get_next_ticketnum();
+	char ticketspath[BUFSIZE];
+	char nextticketpath[BUFSIZE];
+	get_ticketsdir_path(ticketspath);
+	int chars = snprintf(nextticketpath, BUFSIZE, "%s/%d", ticketspath, num);
+	if (chars >= BUFSIZE) {
+		err(EXIT_FAILURE, "Error in getting next ticket path: Buffer size was too small.");
+	} else if (chars < 0) {
+		err(EXIT_FAILURE, "Error in getting next ticket path %s");
+	}
+	FILE* newticketfile = fopen(nextticketpath, "w");
+	if (newticketfile == NULL) {
+		err(EXIT_FAILURE, "Error in opening new ticket file %s");
+	}
+	const char* str = "Test";
+	size_t len = strlen(str);
+	fwrite(str, sizeof(char), len, newticketfile);
+	fclose(newticketfile);
+}
+
+int main1(int argc, const char** argv) {
 	char flags[30];
 	size_t chars = get_flags(argc, argv, flags);
 	if (chars > 30) {
@@ -13,13 +39,12 @@ int main(int argc, const char** argv) {
 		return(1);
 	}
 	for (int i = 0; i < chars; chars++) {
-		if (flags[i] == 'd') {
+		char flag = flags[i];
+		if (flag == 'd') {
 			int ret = create_tickets_dir();
-			return(ret);
-		} else if (flags[i] == 'c') {
+		} else if (flag == 'n') {
 			int ret = get_next_ticketnum();
 			printf("%d\n", ret);
-			return(0);
 		}
 	}
 	return(0);
